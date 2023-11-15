f414f5da7620076db74522f98b4b91c45a8d0828
yalishanda <farbantis@gmail.com>
2023-11-15 18:41:09 +0000

    added csv file

--- data_retrieval.py
+++ data_retrieval.py
@@ -1,10 +1,12 @@
 import pandas as pd
-# datatables = pd.read_html('https://en.wikipedia.org/wiki/Glasgow#Climate')
-# df = datatables[7]  #Glasgow population data
-# print()
+datatables1 = pd.read_html('https://en.wikipedia.org/wiki/Glasgow#Climate')
+df1 = datatables1[7]  #Glasgow population data
+print()
 
+url_csv = "https://raw.githubusercontent.com/futureCodersSE/working-with-data/main/Data%20sets/Paisley-Weather-Data.csv"
+df2 = pd.read_csv(url_csv)
+print(df2)
 
-url = "https://raw.githubusercontent.com/futureCodersSE/working-with-data/main/Data%20sets/Paisley-Weather-Data.csv"
-df = pd.read_csv(url)
-print(df)
+url_excel = "https://github.com/futureCodersSE/working-with-data/blob/main/Data%20sets/public_use-talent-migration.xlsx?raw=true"
+df3 = pd.read_excel(url_excel, sheet_name="Industry Migration")
 
