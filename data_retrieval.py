8c2c85446ecb3ac3b5f68ec22445ffd219dc03a2
yalishanda <farbantis@gmail.com>
2023-11-15 18:54:37 +0000

    first data retrieval from csv

--- data_retrieval.py
+++ data_retrieval.py
@@ -1,12 +1,5 @@
 import pandas as pd
-datatables1 = pd.read_html('https://en.wikipedia.org/wiki/Glasgow#Climate')
-df1 = datatables1[7]  #Glasgow population data
-print()
 
-url_csv = "https://raw.githubusercontent.com/futureCodersSE/working-with-data/main/Data%20sets/Paisley-Weather-Data.csv"
-df2 = pd.read_csv(url_csv)
-print(df2)
-
-url_excel = "https://github.com/futureCodersSE/working-with-data/blob/main/Data%20sets/public_use-talent-migration.xlsx?raw=true"
-df3 = pd.read_excel(url_excel, sheet_name="Industry Migration")
-
+data_from_my_github = "https://raw.githubusercontent.com/farbantis/data-with-python/master/Data_Sets/housing_in_london_yearly_variables.csv"
+df = pd.read_csv(data_from_my_github)
+print(df.head())
