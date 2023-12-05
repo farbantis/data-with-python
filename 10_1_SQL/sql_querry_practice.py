feda28da8756566122744c982d3550f30480a167
yalishanda <farbantis@gmail.com>
2023-12-05 20:15:56 +0000

    finished sql example

--- 10_1_SQL/sql_querry_practice.py
+++ 10_1_SQL/sql_querry_practice.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import psycopg2
+from decimal import Decimal
+from prettytable import PrettyTable
 
 db_user = 'girosman'
 db_password = 'ghjdjlf'
@@ -17,35 +19,30 @@ conn = psycopg2.connect(
 
 cursor = conn.cursor()
 
-# selecting employees with salary > 10_000, concatenating name and surname, add department name
-sql_query_1 = """
-SELECT CONCAT(first_name, ' ', last_name) as full_name, department_name, salary 
-FROM employees
-INNER JOIN departments
-ON employees.department_id = departments.department_id
-WHERE salary > 10000
-ORDER BY salary DESC
-"""
-
 # find max salary for departments for employees hired after 01/01/97
-sql_query_02 = """
-SELECT department_name, MAX(salary) as max_salary, MIN(hire_date) as earlierst_date_hired
-FROM employees
-INNER JOIN departments
-ON employees.department_id = departments.department_id
-WHERE hire_date > '1997-01-01'
-GROUP BY department_name
-ORDER BY MAX(salary) DESC
-"""
+# sql_query = """
+# SELECT department_name, MAX(salary) as max_salary, MIN(hire_date) as earlierst_date_hired
+# FROM employees
+# INNER JOIN departments
+# ON employees.department_id = departments.department_id
+# WHERE hire_date > '1997-01-01'
+# GROUP BY department_name
+# ORDER BY MAX(salary) DESC
+# """
 
-# find all people working in a department with max salary, showing their full name, dep, job title, salary
+# find all people working in a department with min salary, showing their full name, dep, job title, salary
 # with adding a field to comment salary level
 sql_query = """
-SELECT first_name, last_name, department_name, job_title, salary, 
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    department_name,
+    INITCAP(REPLACE(LOWER(job_title), 'purchasing', '')) AS position,
+    salary,
+    salary * 12 AS salary_per_year,  
     CASE 
-        WHEN salary < 10000 THEN 'low salary'
-        WHEN salary BETWEEN 10000 AND 20000 THEN 'middle salary'
-        WHEN salary > 20000 THEN 'high salary'
+        WHEN salary < 3000 THEN 'low salary'
+        WHEN salary BETWEEN 3000 AND 5000 THEN 'middle salary'
+        WHEN salary > 50000 THEN 'high salary'
         ELSE 'wrong data'
     END as comment_to_salary    
 FROM employees
@@ -57,11 +54,9 @@ WHERE employees.department_id = (
     SELECT department_id
     FROM employees
     GROUP BY employees.department_id
-    ORDER BY MAX(salary) DESC
+    ORDER BY MIN(salary) ASC
     LIMIT 1)
 """
-
-
 
 cursor.execute(sql_query)
 conn.commit()
@@ -71,5 +66,15 @@ conn.close()
 
 columns = [desc[0] for desc in cursor.description]
 df = pd.DataFrame(result, columns=columns)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+df['salary_after_tax'] = (df['salary'] - 1200) * Decimal('0.8') + 1200
+position = df.columns.get_loc('salary') + 1
+df.insert(position, 'salary_after_tax', df.pop('salary_after_tax'))
 
-print(df.head(50))
+table = PrettyTable()
+table.field_names = df.columns
+for row in df.itertuples(index=False):
+    table.add_row(row)
+print(table)
+#print(df.head(50))
