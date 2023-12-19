8e18d1f6e7821c6bb613c208b67ec44c82c8f939
yalishanda <farbantis@gmail.com>
2023-12-19 21:05:11 +0000

    elections finished

--- assessment/cleaning_data_election.py
+++ assessment/cleaning_data_election.py
@@ -2,18 +2,21 @@ import pandas as pd
 
 
 def elections(url):
-    # from df.info() we can learn that
-    # national_voteshare_3rd_lo column is totally empty, we dont need it
-    # 3 columns contain inconsistent data: national_turnout, national_turnout_hi, national_turnout_lo
-    # df = df[df['national_turnout'].isna()] shows that all 3 columns go with blank data in same rows
-    # we can delete these rows
+    """
+    from df.info() we can learn that national_voteshare_3rd_lo column is totally empty, we dont need it
+    3 columns contain inconsistent data: national_turnout, national_turnout_hi, national_turnout_lo
+    timestamp is not a Datetime object
+    df = df[df['national_turnout'].isna()] shows that all 3 columns go with blank data in same rows - we
+    can delete those rows if they take part in calculations
+    """
     df = pd.read_csv(url)
-    #df.columns = df.columns.str.strip()
+    print(df.info())
+    # df.columns = df.columns.str.strip()
     df.drop('national_voteshare_3rd_lo', axis=1, inplace=True)
+    df['timestamp'] = pd.to_datetime(df['timestamp'], format='%d/%m/%Y %H:%M')
     print(df.info())
     # pd.set_option('display.max_columns', None)
-    #df = df[df['national_turnout'].isna()]
-    df.dropna(subset=['national_turnout', 'national_turnout_hi', 'national_turnout_lo'], inplace=True)
+    # df.dropna(subset=['national_turnout', 'national_turnout_hi', 'national_turnout_lo'], inplace=True)
     return df
 
 
